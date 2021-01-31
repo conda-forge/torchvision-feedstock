18adfb882e4efd3c566b651fe7b344868f66768b
Isuru Fernando <isuruf@gmail.com>
2021-01-30 19:33:55 -0600

    FORCE_CUDA

--- recipe/build-torch.sh
+++ recipe/build-torch.sh
@@ -1,2 +1,8 @@
+if [[ "$cuda_compiler_version" == "None" ]]; then
+  export FORCE_CUDA=0
+else
+  export FORCE_CUDA=1
+fi
+
 export TORCHVISION_INCLUDE="${PREFIX}/include/"
 ${PYTHON} -m pip install . -vv
