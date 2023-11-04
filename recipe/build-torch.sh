7b68106b42e5659c1c5bdf3f082c7373e91f2420
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-04 14:37:42 -0400

    Update build

--- recipe/build-torch.sh
+++ recipe/build-torch.sh
@@ -14,6 +14,10 @@ else
       export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
   elif [[ ${cuda_compiler_version} == 11.2* ]]; then
       export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+  elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+      export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+  elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
   else
       echo "unsupported cuda version. edit build.sh"
       exit 1
