c2cfa85f2ac124d888e7e46526f247f5b20a1019
H. Vetinari <h.vetinari@gmx.com>
2025-07-26 12:16:55 +1100

    update CUDA arches

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,16 +3,14 @@ set -ex
 if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
-  if [[ ${cuda_compiler_version} == 12.6 ]]; then
-      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0"
+  if [[ ${cuda_compiler_version} == 12.9 ]]; then
+      export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;7.5;8.0;8.6;8.9;9.0;10.0;12.0+PTX"
       # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
       export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
   else
       echo "unsupported cuda version. edit build.sh"
       exit 1
   fi
-  # Add PTX at the end, always
-  export TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}+PTX"
   export FORCE_CUDA=1
 fi
 
