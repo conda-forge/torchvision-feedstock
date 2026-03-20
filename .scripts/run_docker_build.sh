f21aee2918ff303b8b4234534213a79cb6bd5e14
Ruben Arts <ruben.arts@hotmail.com>
2026-03-20 13:37:21 +0100

    Add tegra arm support

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -93,6 +93,7 @@ docker run ${DOCKER_RUN_ARGS} \
            -e GIT_BRANCH \
            -e UPLOAD_ON_BRANCH \
            -e CI \
+           -e GITHUB_ACTIONS \
            -e FEEDSTOCK_NAME \
            -e CPU_COUNT \
            -e BUILD_WITH_CONDA_DEBUG \
